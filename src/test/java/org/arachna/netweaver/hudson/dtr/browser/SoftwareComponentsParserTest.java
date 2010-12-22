4aaa6026f74e2a27766559518d91730a8263f4e3
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2010-12-22 23:10:06 +0100

    fix SoftwareComponentsParserTest.

--- src/test/java/org/arachna/netweaver/hudson/dtr/browser/SoftwareComponentsParserTest.java
+++ src/test/java/org/arachna/netweaver/hudson/dtr/browser/SoftwareComponentsParserTest.java
@@ -23,9 +23,29 @@ import org.junit.Test;
  */
 public class SoftwareComponentsParserTest {
     /**
+     * 3rd example SC.
+     */
+    private static final String SC3 = "example.com_EXAMPLE_SC3_1";
+
+    /**
+     * 2nd example SC.
+     */
+    private static final String SC2 = "example.com_EXAMPLE_SC2_1";
+
+    /**
+     * 1st example SC.
+     */
+    private static final String SC1 = "example.com_EXAMPLE_SC1_1";
+
+    /**
+     * example vendor.
+     */
+    private static final String EXAMPLE_COM = "example.com";
+
+    /**
      * expected compartment names.
      */
-    private static final String[] COMPARTMENT_NAMES = new String[] { "EXAMPLE_SC1", "EXAMPLE_SC2", "EXAMPLE_SC3" };
+    private static final String[] COMPARTMENT_NAMES = new String[] { SC1, SC2, SC3 };
 
     /**
      * Mapping for compartments read from example output of compartment list of
@@ -38,12 +58,13 @@ public class SoftwareComponentsParserTest {
     @Before
     public void setUp() {
         this.compartments.clear();
+        final DevelopmentConfiguration config = new DevelopmentConfiguration("Example");
+        config.add(new Compartment(SC1, CompartmentState.Source, EXAMPLE_COM, SC1, "EXAMPLE_SC1"));
+        config.add(new Compartment(SC2, CompartmentState.Source, EXAMPLE_COM, SC2, "EXAMPLE_SC2"));
+        config.add(new Compartment(SC3, CompartmentState.Source, EXAMPLE_COM, SC3, "EXAMPLE_SC3"));
+
         final InputStream input = this.getClass().getResourceAsStream("ExampleTrack.htm");
         final SoftwareComponentsParser browser = new SoftwareComponentsParser();
-        final DevelopmentConfiguration config = new DevelopmentConfiguration("Example");
-        config.add(new Compartment("EXAMPLE_SC1", CompartmentState.Source, "example.com", "", "EXAMPLE_SC1_1"));
-        config.add(new Compartment("EXAMPLE_SC2", CompartmentState.Source, "example.com", "", "EXAMPLE_SC2_1"));
-        config.add(new Compartment("EXAMPLE_SC3", CompartmentState.Source, "example.com", "", "EXAMPLE_SC3_1"));
 
         for (final Compartment compartment : browser.parse(input, config)) {
             this.compartments.put(compartment.getName(), compartment);
@@ -62,7 +83,7 @@ public class SoftwareComponentsParserTest {
             compartment = compartments.get(compartmentName);
 
             assertNotNull(compartment);
-            assertEquals("example.com", compartment.getVendor());
+            assertEquals(EXAMPLE_COM, compartment.getVendor());
             assertEquals(compartmentName, compartment.getName());
         }
     }
