a39c1cd796cf4e9ee0797e90a723a07936e01b55
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2011-02-13 22:45:39 +0100

    improve formatting.

--- src/test/java/org/arachna/netweaver/hudson/nwdi/BuildDevelopmentComponentsCommandBuilderTest.java
+++ src/test/java/org/arachna/netweaver/hudson/nwdi/BuildDevelopmentComponentsCommandBuilderTest.java
@@ -32,7 +32,8 @@ public final class BuildDevelopmentComponentsCommandBuilderTest {
     private DevelopmentComponentFactory dcFactory;
 
     /**
-     * Set up development configuration, compartment and development components to be used in test.
+     * Set up development configuration, compartment and development components
+     * to be used in test.
      */
     @Before
     public void setUp() {
@@ -50,7 +51,8 @@ public final class BuildDevelopmentComponentsCommandBuilderTest {
      */
     protected List<String> createBuildCommands(final DevelopmentComponent[] components) {
         final DevelopmentConfiguration config = new DevelopmentConfiguration("DI0_Example_D");
-        final Compartment compartment = new Compartment(EXAMPLE_SC, CompartmentState.Source, "example.com", "", "EXAMPLE_SC");
+        final Compartment compartment =
+            new Compartment(EXAMPLE_SC, CompartmentState.Source, "example.com", "", "EXAMPLE_SC");
         config.add(compartment);
         final Collection<DevelopmentComponent> dCs = Arrays.asList(components);
         compartment.add(dCs);
@@ -59,16 +61,18 @@ public final class BuildDevelopmentComponentsCommandBuilderTest {
     }
 
     /**
-     * Test method for {@link org.arachna.netweaver.dc.analyzer.dctool.BuildDevelopmentComponentsCommandBuilder#execute()} .
+     * Test method for
+     * {@link org.arachna.netweaver.dc.analyzer.dctool.BuildDevelopmentComponentsCommandBuilder#execute()}
+     * .
      */
     @Test
     public void testBuildDcCommandGeneration() {
         final List<String> commands =
-            this.createBuildCommands(new DevelopmentComponent[] { this.dcFactory.get(ExampleDevelopmentComponentFactory.EXAMPLE_COM,
-                ExampleDevelopmentComponentFactory.LIB_JETM) });
+            this.createBuildCommands(new DevelopmentComponent[] { this.dcFactory.get(
+                ExampleDevelopmentComponentFactory.EXAMPLE_COM, ExampleDevelopmentComponentFactory.LIB_JETM) });
         final String[] expected =
-            new String[] { String.format("builddc -s %s -n %s -v %s -o;", EXAMPLE_SC, ExampleDevelopmentComponentFactory.LIB_JETM,
-                ExampleDevelopmentComponentFactory.EXAMPLE_COM) };
+            new String[] { String.format("builddc -s %s -n %s -v %s -o;", EXAMPLE_SC,
+                ExampleDevelopmentComponentFactory.LIB_JETM, ExampleDevelopmentComponentFactory.EXAMPLE_COM) };
         assertThat(commands, hasSize(expected.length));
     }
 }
