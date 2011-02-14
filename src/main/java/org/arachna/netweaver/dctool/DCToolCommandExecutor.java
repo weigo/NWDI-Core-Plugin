496670355902cd92633e92bc1651086d87992efa
weigo <Dirk.Weigenand@gmx.de>
2011-02-14 22:35:00 +0100

    small clean up

--- src/main/java/org/arachna/netweaver/dctool/DCToolCommandExecutor.java
+++ src/main/java/org/arachna/netweaver/dctool/DCToolCommandExecutor.java
@@ -13,6 +13,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -71,21 +72,19 @@ public final class DCToolCommandExecutor {
     private final DCToolDescriptor dcToolDescriptor;
 
     /**
-     * create DC tool executor with the given command line generator and given
-     * command build.
+     * create DC tool executor with the given command line generator and given command build.
      * 
      * @param launcher
      *            the launcher to use executing the DC tool.
      * @param workspace
      *            the workspace where the DC tool should be executed.
      * @param dcToolDescriptor
-     *            descriptor for various parameters needed for DC tool
-     *            execution.
+     *            descriptor for various parameters needed for DC tool execution.
      * @param developmentConfiguration
      *            {@link DevelopmentConfiguration} to use executing the DC tool.
      */
-    public DCToolCommandExecutor(final Launcher launcher, final FilePath workspace,
-        final DCToolDescriptor dcToolDescriptor, final DevelopmentConfiguration developmentConfiguration) {
+    public DCToolCommandExecutor(final Launcher launcher, final FilePath workspace, final DCToolDescriptor dcToolDescriptor,
+        final DevelopmentConfiguration developmentConfiguration) {
         this.launcher = launcher;
         this.workspace = workspace;
         this.dcToolDescriptor = dcToolDescriptor;
@@ -93,18 +92,17 @@ public final class DCToolCommandExecutor {
     }
 
     /**
-     * Execute dctool with the given {@link DCToolCommandBuilder}.
+     * Execute dc tool with the given {@link DCToolCommandBuilder}.
      * 
      * @param commandBuilder
      *            builder for dc tool commands
-     * @return content of logfile created by the executed dctool.
+     * @return content of log file created by the executed dc tool.
      * @throws IOException
      *             when an error occurred writing the the command file.
      * @throws InterruptedException
      *             when the user canceled the action.
      */
-    public DcToolCommandExecutionResult execute(final DCToolCommandBuilder commandBuilder) throws IOException,
-        InterruptedException {
+    public DcToolCommandExecutionResult execute(final DCToolCommandBuilder commandBuilder) throws IOException, InterruptedException {
         final List<String> commands = commandBuilder.execute();
         final ByteArrayOutputStream result = new ByteArrayOutputStream();
         int exitCode = 0;
@@ -130,11 +128,11 @@ public final class DCToolCommandExecutor {
      *            list of dc tool commands
      * @return <code>InputStream</code> containing the given dc tool commands.
      */
-    private ByteArrayInputStream createCommandInputStream(final List<String> commands) {
+    private InputStream createCommandInputStream(final List<String> commands) {
         final StringBuilder cmds = new StringBuilder();
         cmds.append("exectime -m on;\n");
-        cmds.append(String.format(LOAD_CONFIG_COMMAND, this.dcToolDescriptor.getUser(),
-            this.dcToolDescriptor.getPassword(), this.dcToolDescriptor.getDtrDirectory(), ".dtc"));
+        cmds.append(String.format(LOAD_CONFIG_COMMAND, this.dcToolDescriptor.getUser(), this.dcToolDescriptor.getPassword(),
+            this.dcToolDescriptor.getDtrDirectory(), ".dtc"));
 
         for (final String cmd : commands) {
             cmds.append(cmd).append('\n');
@@ -151,8 +149,7 @@ public final class DCToolCommandExecutor {
      * @param isUnix
      *            indicate whether to run on a unixoid OS or Windows.
      * @param commandFile
-     *            <code>FilePath</code> where DC tool commands should be written
-     *            to.
+     *            <code>FilePath</code> where DC tool commands should be written to.
      * @return the created command line.
      */
     protected ArgumentListBuilder createDcToolCommand(final boolean isUnix, final FilePath commandFile) {
@@ -172,16 +169,14 @@ public final class DCToolCommandExecutor {
      * Generate the fully qualified command to be used to execute the dc tool.
      * 
      * @param isUnix
-     *            indicate whether the platform to run on is Unix(oid) or
-     *            Windows.
+     *            indicate whether the platform to run on is Unix(oid) or Windows.
      * @return fully qualified command to be used to execute the dc tool.
      */
     private String getFullyQualifiedDcToolCommand(final boolean isUnix) {
         final String command = isUnix ? "dctool.sh" : "dctool.bat";
         final Character pathSeparator = isUnix ? '/' : '\\';
-        final String fullyQualifiedDcToolCommand =
-            String.format("%s%c%s", this.getDcToolPath(), pathSeparator, command);
-        return fullyQualifiedDcToolCommand;
+
+        return String.format("%s%c%s", this.getDcToolPath(), pathSeparator, command);
     }
 
     /**
@@ -199,8 +194,7 @@ public final class DCToolCommandExecutor {
     /**
      * Prepare the environment variables for the launcher.
      * 
-     * @return the map containing the environment variable name mapping to their
-     *         corresponding values.
+     * @return the map containing the environment variable name mapping to their corresponding values.
      */
     protected Map<String, String> createEnvironment() {
         final Map<String, String> environment = new HashMap<String, String>();
@@ -223,8 +217,7 @@ public final class DCToolCommandExecutor {
      */
     protected JdkHomeAlias getJdkPropertyName() {
         String jdkPropertyName = "";
-        final String pathKey =
-            this.developmentConfiguration.getBuildVariant().getBuildOption(COM_SAP_JDK_HOME_PATH_KEY);
+        final String pathKey = this.developmentConfiguration.getBuildVariant().getBuildOption(COM_SAP_JDK_HOME_PATH_KEY);
 
         if (pathKey != null) {
             jdkPropertyName = pathKey;
@@ -234,9 +227,8 @@ public final class DCToolCommandExecutor {
     }
 
     /**
-     * Get JDK home path. Try to find a JDK location from the current build
-     * variant. Returns the location configured in the JdkHomePaths, the value
-     * of the java.home property when no value could be found.
+     * Get JDK home path. Try to find a JDK location from the current build variant. Returns the location configured in the JdkHomePaths,
+     * the value of the java.home property when no value could be found.
      * 
      * @return a JDK location for executing the dctool with.
      */
