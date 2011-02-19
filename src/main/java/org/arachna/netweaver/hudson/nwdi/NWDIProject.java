58fc854bb7b7e55efb4b639307e0dc4dcc6cd0e8
weigo <Dirk.Weigenand@gmx.de>
2011-02-19 14:01:32 +0100

    add unit test for NWDIProject.DescriptorImpl.configure()

--- src/main/java/org/arachna/netweaver/hudson/nwdi/NWDIProject.java
+++ src/main/java/org/arachna/netweaver/hudson/nwdi/NWDIProject.java
@@ -38,7 +38,7 @@ import org.kohsuke.stapler.StaplerResponse;
  * 
  * @author Dirk Weigenand
  */
-public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implements TopLevelItem {
+public class NWDIProject extends Project<NWDIProject, NWDIBuild> implements TopLevelItem {
     /**
      * parameter name for project configuration controlling whether the
      * workspace should be clean before building.
@@ -170,7 +170,30 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
      * 
      * @author Dirk Weigenand
      */
-    public static final class DescriptorImpl extends AbstractProjectDescriptor {
+    public static class DescriptorImpl extends AbstractProjectDescriptor {
+        /**
+         * Constant for password request parameter.
+         */
+        protected static final String NWDI_PLUGIN_PASSWORD = "NWDIPlugin.password";
+
+        /**
+         * Constant for user request parameter.
+         */
+        protected static final String NWDI_PLUGIN_USER = "NWDIPlugin.user";
+
+        /**
+         * Constant for nwdi tool libraries folder request parameter.
+         */
+        protected static final String NWDI_PLUGIN_NWDI_TOOL_LIB_FOLDER = "NWDIPlugin.nwdiToolLibFolder";
+
+        /**
+         * Constant for JDK home paths request parameter.
+         */
+        protected static final String NWDI_PLUGIN_JDK_HOME_PATHS = "NWDIPlugin.jdkHomePaths";
+
+        /**
+         * Constant for dc tool sub folder in nwdi tool library folder.
+         */
         private static final String DC_SUB_FOLDER = "dc";
 
         /**
@@ -189,14 +212,9 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
         private String nwdiToolLibFolder;
 
         /**
-         * path to the 'JDK_1_3_1_HOME' installation.
+         * 'JDK_HOME_PATHS'.
          */
-        private String jdk131Home;
-
-        /**
-         * path to the 'JDK_1_4_2_HOME' installation.
-         */
-        private String jdk142Home;
+        private String jdkHomePaths;
 
         /**
          * Create descriptor for NWDI-Projects and load global configuration
@@ -252,33 +270,20 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
         }
 
         /**
-         * @return the jdk131Home
+         * @return the JDK_HOME_PATHS
          */
-        public String getJdk131Home() {
-            return jdk131Home;
+        public String getJdkHomePaths() {
+            return jdkHomePaths;
         }
 
         /**
-         * @param jdk131Home
-         *            the jdk131Home to set
+         * Set the JDK_HOME_PATHS.
+         * 
+         * @param jdkHomePaths
+         *            the jdkHomePaths to set
          */
-        public void setJdk131Home(final String jdk131Home) {
-            this.jdk131Home = jdk131Home;
-        }
-
-        /**
-         * @return the jdk142Home
-         */
-        public String getJdk142Home() {
-            return jdk142Home;
-        }
-
-        /**
-         * @param jdk142Home
-         *            the jdk142Home to set
-         */
-        public void setJdk142Home(final String jdk142Home) {
-            this.jdk142Home = jdk142Home;
+        public void setJdkHomePaths(final String jdkHomePaths) {
+            this.jdkHomePaths = jdkHomePaths;
         }
 
         /*
@@ -290,11 +295,12 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
          */
         @Override
         public boolean configure(final StaplerRequest req, final JSONObject json) throws FormException {
-            this.jdk131Home = getParameter(req, "NWDIPlugin.jdk131Home");
-            this.jdk142Home = getParameter(req, "NWDIPlugin.jdk142Home");
-            this.nwdiToolLibFolder = getParameter(req, "NWDIPlugin.nwdiToolLibFolder");
-            this.user = getParameter(req, "NWDIPlugin.user");
-            this.password = getParameter(req, "NWDIPlugin.password");
+            final ParameterHelper helper = new ParameterHelper(req);
+
+            this.jdkHomePaths = helper.getParameter(NWDI_PLUGIN_JDK_HOME_PATHS);
+            this.nwdiToolLibFolder = helper.getParameter(NWDI_PLUGIN_NWDI_TOOL_LIB_FOLDER);
+            this.user = helper.getParameter(NWDI_PLUGIN_USER);
+            this.password = helper.getParameter(NWDI_PLUGIN_PASSWORD);
 
             save();
 
@@ -358,6 +364,10 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
                 .error(Messages.NWDIProject_NwdiToolLibFolder_no_proper_installation_folder(DC_SUB_FOLDER));
         }
 
+        protected FormValidation doJdkHomePathsCheck() {
+            return FormValidation.error("");
+        }
+
         /**
          * Returns the requested parameter from the given {@link StaplerRequest}
          * .
@@ -371,7 +381,7 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
          *         <code>StaplerRequest</code>.
          */
         private String getParameter(final StaplerRequest req, final String parameter) {
-            return Util.fixEmpty(req.getParameter(parameter).trim());
+            return Util.fixEmptyAndTrim(req.getParameter(parameter));
         }
 
         /**
@@ -406,8 +416,15 @@ public final class NWDIProject extends Project<NWDIProject, NWDIBuild> implement
         JdkHomePaths getConfiguredJdkHomePaths() {
             final JdkHomePaths paths = new JdkHomePaths();
 
-            paths.add(JdkHomeAlias.Jdk131Home, this.getJdk131Home());
-            paths.add(JdkHomeAlias.Jdk142Home, this.getJdk142Home());
+            for (String pathDef : this.getJdkHomePaths().split(",;")) {
+                final String[] parts = pathDef.split("=");
+
+                final JdkHomeAlias alias = JdkHomeAlias.valueOf(parts[0]);
+
+                if (alias != null) {
+                    paths.add(alias, parts[1]);
+                }
+            }
 
             return paths;
         }
---
+++ src/test/java/org/arachna/netweaver/hudson/nwdi/NWDIProjectTest.java
@@ -0,0 +1,295 @@
+/**
+ *
+ */
+package org.arachna.netweaver.hudson.nwdi;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import hudson.model.Descriptor.FormException;
+import hudson.model.Item;
+import hudson.model.ItemGroup;
+import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
+import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.search.SearchIndex;
+import hudson.search.Search;
+import hudson.security.ACL;
+import hudson.security.Permission;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import net.sf.json.JSONObject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Unit tests for {@link NWDIProject}.
+ * 
+ * @author Dirk Weigenand
+ */
+public class NWDIProjectTest implements TopLevelItem {
+    /**
+     * Descriptor under test.
+     */
+    private NWDIProject.DescriptorImpl descriptor;
+
+    /**
+     * Initialize empty descriptor.
+     */
+    @Before
+    public void setUp() {
+        this.descriptor = new NWDIProject.DescriptorImpl() {
+            @Override
+            public synchronized void load() {
+            }
+
+            @Override
+            public synchronized void save() {
+            }
+        };
+
+    }
+
+    /**
+     * Reset descriptor.
+     */
+    @After
+    public void tearDown() {
+        this.descriptor = null;
+    }
+
+    /**
+     * Test
+     * {@link NWDIProject.DescriptorImpl#configure(StaplerRequest, JSONObject)}.
+     * 
+     * @throws FormException
+     */
+    @Test
+    public void testDescriptorImplConfigureWithEmptyRequest() throws FormException {
+        final StaplerRequest request = new FakeStaplerRequest(new String[] {}, new String[] {});
+
+        this.descriptor.configure(request, null);
+
+        assertThat(this.descriptor.getJdkHomePaths(), equalTo(""));
+        assertThat(this.descriptor.getNwdiToolLibFolder(), equalTo(""));
+        assertThat(this.descriptor.getUser(), equalTo(""));
+        assertThat(this.descriptor.getPassword(), equalTo(""));
+    }
+
+    /**
+     * Test
+     * {@link NWDIProject.DescriptorImpl#configure(StaplerRequest, JSONObject)}.
+     * 
+     * @throws FormException
+     */
+    @Test
+    public void testDescriptorImplConfigureWithParameters() throws FormException {
+        final String jdk16 = "/opt/jdk1.6";
+        final String nwdiToolsFolder = "/opt/nwdi/lib";
+        final String user = "user";
+        final String password = "secret";
+
+        final StaplerRequest request =
+            new FakeStaplerRequest(new String[] { NWDIProject.DescriptorImpl.NWDI_PLUGIN_JDK_HOME_PATHS,
+                NWDIProject.DescriptorImpl.NWDI_PLUGIN_NWDI_TOOL_LIB_FOLDER,
+                NWDIProject.DescriptorImpl.NWDI_PLUGIN_USER, NWDIProject.DescriptorImpl.NWDI_PLUGIN_PASSWORD },
+                new String[] { jdk16, nwdiToolsFolder, user, password });
+
+        this.descriptor.configure(request, null);
+
+        assertThat(this.descriptor.getJdkHomePaths(), equalTo(jdk16));
+        assertThat(this.descriptor.getNwdiToolLibFolder(), equalTo(nwdiToolsFolder));
+        assertThat(this.descriptor.getUser(), equalTo(user));
+        assertThat(this.descriptor.getPassword(), equalTo(password));
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public Collection<? extends Job> getAllJobs() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getFullName() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getDisplayName() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getFullDisplayName() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getUrl() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getShortUrl() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getAbsoluteUrl() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void onLoad(final ItemGroup<? extends Item> parent, final String name) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void onCopiedFrom(final Item src) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void onCreatedFromScratch() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void save() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void delete() throws IOException, InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public File getRootDir() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public Search getSearch() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getSearchName() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public String getSearchUrl() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public SearchIndex getSearchIndex() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public ACL getACL() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public void checkPermission(final Permission permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public boolean hasPermission(final Permission permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public Hudson getParent() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Empty implementation. Throws {@link UnsupportedOperationException}.
+     * {@inheritDoc}
+     */
+    public TopLevelItemDescriptor getDescriptor() {
+        throw new UnsupportedOperationException();
+    }
+}
