ec5cd53fc5327c13b73c7e378c1cdb96813e8e9d
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2011-02-27 23:10:32 +0100

    add test data for PublicPart(s)Reader.

---
+++ src/test/resources/org/arachna/netweaver/hudson/nwdi/dcupdater/API.pp
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<public-part
+         xmlns="http://xml.sap.com/2002/11/PublicPart"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://xml.sap.com/2002/11/PublicPart ppdef.xsd"
+         version="1.0.2"
+         xmlns:IDX="urn:sap.com:PublicPart:1.0">
+    <name>API</name>
+    <description>description</description>
+    <caption>caption</caption>
+    <purpose>compilation</purpose>
+    <entities>
+        <entity>
+            <name>Portal API</name>
+            <entity-type>Portal API</entity-type>
+        </entity>
+    </entities>
+</public-part>
