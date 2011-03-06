39d93e30dc002a74f9f8452ad8275fb50ad8d451
G526521 <G526521@PN-GISA-45050.gisa-halle.de>
2011-03-07 00:00:03 +0100

    handle cases where another component has set the System property wrt. where XMLReaders shall be instantiated from.

--- src/main/java/org/arachna/xml/XmlReaderHelper.java
+++ src/main/java/org/arachna/xml/XmlReaderHelper.java
@@ -9,6 +9,7 @@ import java.io.Reader;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
@@ -45,10 +46,35 @@ public final class XmlReaderHelper {
      *             when reading the XML input fails.
      */
     public void parse(final Reader input) throws IOException, SAXException {
-        final XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+        final XMLReader xmlReader = createXmlReader();
         xmlReader.setContentHandler(this.handler);
         this.handler.setXmlReader(xmlReader);
 
         xmlReader.parse(new InputSource(input));
+    }
+
+    /**
+     * @return
+     * @throws SAXException
+     */
+    private XMLReader createXmlReader() throws SAXException {
+        XMLReader xmlReader = null;
+
+        try {
+            xmlReader = XMLReaderFactory.createXMLReader();
+        }
+        catch (final RuntimeException re) {
+            if (re.getCause() != null && ClassNotFoundException.class.equals(re.getCause().getClass())) {
+                final String saxDriverProperty = System.getProperty("org.xml.sax.driver");
+
+                if (saxDriverProperty != null) {
+                    System.getProperty("org.xml.sax.driver", null);
+                }
+
+                xmlReader = XMLReaderFactory.createXMLReader();
+            }
+        }
+
+        return xmlReader;
     }
 }
