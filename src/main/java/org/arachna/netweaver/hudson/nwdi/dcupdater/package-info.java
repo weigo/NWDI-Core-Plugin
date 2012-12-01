604447414cbb341730fe6fd52155ef5334eb3258
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2012-12-01 23:34:44 +0100

    convert package to package-info.java

---
+++ src/main/java/org/arachna/netweaver/hudson/nwdi/dcupdater/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>Contains classes for reading various configuration files of
+ * development components.</p>
+ * <p>The information read from those configuration files is added to
+ * the development configuration and its respective development components.
+ * </p>
+ */
+package org.arachna.netweaver.hudson.nwdi.dcupdater;
