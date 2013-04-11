75e263570dc8eceb7e1218bbe332dc63703de7c5
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2013-04-11 21:16:54 +0200

    Streamline initialization of class names in 'java.lang' package.

--- src/main/java/org/arachna/javaparser/ClassNameResolver.java
+++ src/main/java/org/arachna/javaparser/ClassNameResolver.java
@@ -28,101 +28,29 @@ public class ClassNameResolver {
     @SuppressWarnings("serial")
     private static final Map<String, String> JAVA_LANG = new HashMap<String, String>() {
         {
-            put("Appendable", "java.lang.Appendable");
-            put("CharSequence", "java.lang.CharSequence");
-            put("Cloneable", "java.lang.Cloneable");
-            put("Comparable", "java.lang.Comparable");
-            put("Iterable", "java.lang.Iterable");
-            put("Readable", "java.lang.Readable");
-            put("Runnable", "java.lang.Runnable");
-            put("Thread.UncaughtExceptionHandler", "java.lang.Thread.UncaughtExceptionHandler");
-            put("Boolean", "java.lang.Boolean");
-            put("Byte", "java.lang.Byte");
-            put("Character", "java.lang.Character");
-            put("Character.Subset", "java.lang.Character.Subset");
-            put("Character.UnicodeBlock", "java.lang.Character.UnicodeBlock");
-            put("Class", "java.lang.Class");
-            put("ClassLoader", "java.lang.ClassLoader");
-            put("Compiler", "java.lang.Compiler");
-            put("Double", "java.lang.Double");
-            put("Enum", "java.lang.Enum");
-            put("Float", "java.lang.Float");
-            put("InheritableThreadLocal", "java.lang.InheritableThreadLocal");
-            put("Integer", "java.lang.Integer");
-            put("Long", "java.lang.Long");
-            put("Math", "java.lang.Math");
-            put("Number", "java.lang.Number");
-            put("Object", "java.lang.Object");
-            put("Package", "java.lang.Package");
-            put("Process", "java.lang.Process");
-            put("ProcessBuilder", "java.lang.ProcessBuilder");
-            put("Runtime", "java.lang.Runtime");
-            put("RuntimePermission", "java.lang.RuntimePermission");
-            put("SecurityManager", "java.lang.SecurityManager");
-            put("Short", "java.lang.Short");
-            put("StackTraceElement", "java.lang.StackTraceElement");
-            put("StrictMath", "java.lang.StrictMath");
-            put("String", "java.lang.String");
-            put("StringBuffer", "java.lang.StringBuffer");
-            put("StringBuilder", "java.lang.StringBuilder");
-            put("System", "java.lang.System");
-            put("Thread", "java.lang.Thread");
-            put("ThreadGroup", "java.lang.ThreadGroup");
-            put("ThreadLocal", "java.lang.ThreadLocal");
-            put("Throwable", "java.lang.Throwable");
-            put("Void", "java.lang.Void");
-            put("Thread.State", "java.lang.Thread.State");
-            put("ArithmeticException", "java.lang.ArithmeticException");
-            put("ArrayIndexOutOfBoundsException", "java.lang.ArrayIndexOutOfBoundsException");
-            put("ArrayStoreException", "java.lang.ArrayStoreException");
-            put("ClassCastException", "java.lang.ClassCastException");
-            put("ClassNotFoundException", "java.lang.ClassNotFoundException");
-            put("CloneNotSupportedException", "java.lang.CloneNotSupportedException");
-            put("EnumConstantNotPresentException", "java.lang.EnumConstantNotPresentException");
-            put("Exception", "java.lang.Exception");
-            put("IllegalAccessException", "java.lang.IllegalAccessException");
-            put("IllegalArgumentException", "java.lang.IllegalArgumentException");
-            put("IllegalMonitorStateException", "java.lang.IllegalMonitorStateException");
-            put("IllegalStateException", "java.lang.IllegalStateException");
-            put("IllegalThreadStateException", "java.lang.IllegalThreadStateException");
-            put("IndexOutOfBoundsException", "java.lang.IndexOutOfBoundsException");
-            put("InstantiationException", "java.lang.InstantiationException");
-            put("InterruptedException", "java.lang.InterruptedException");
-            put("NegativeArraySizeException", "java.lang.NegativeArraySizeException");
-            put("NoSuchFieldException", "java.lang.NoSuchFieldException");
-            put("NoSuchMethodException", "java.lang.NoSuchMethodException");
-            put("NullPointerException", "java.lang.NullPointerException");
-            put("NumberFormatException", "java.lang.NumberFormatException");
-            put("RuntimeException", "java.lang.RuntimeException");
-            put("SecurityException", "java.lang.SecurityException");
-            put("StringIndexOutOfBoundsException", "java.lang.StringIndexOutOfBoundsException");
-            put("TypeNotPresentException", "java.lang.TypeNotPresentException");
-            put("UnsupportedOperationException", "java.lang.UnsupportedOperationException");
-            put("AbstractMethodError", "java.lang.AbstractMethodError");
-            put("AssertionError", "java.lang.AssertionError");
-            put("ClassCircularityError", "java.lang.ClassCircularityError");
-            put("ClassFormatError", "java.lang.ClassFormatError");
-            put("Error", "java.lang.Error");
-            put("ExceptionInInitializerError", "java.lang.ExceptionInInitializerError");
-            put("IllegalAccessError", "java.lang.IllegalAccessError");
-            put("IncompatibleClassChangeError", "java.lang.IncompatibleClassChangeError");
-            put("InstantiationError", "java.lang.InstantiationError");
-            put("InternalError", "java.lang.InternalError");
-            put("LinkageError", "java.lang.LinkageError");
-            put("NoClassDefFoundError", "java.lang.NoClassDefFoundError");
-            put("NoSuchFieldError", "java.lang.NoSuchFieldError");
-            put("NoSuchMethodError", "java.lang.NoSuchMethodError");
-            put("OutOfMemoryError", "java.lang.OutOfMemoryError");
-            put("StackOverflowError", "java.lang.StackOverflowError");
-            put("ThreadDeath", "java.lang.ThreadDeath");
-            put("UnknownError", "java.lang.UnknownError");
-            put("UnsatisfiedLinkError", "java.lang.UnsatisfiedLinkError");
-            put("UnsupportedClassVersionError", "java.lang.UnsupportedClassVersionError");
-            put("VerifyError", "java.lang.VerifyError");
-            put("VirtualMachineError", "java.lang.VirtualMachineError");
-            put("Deprecated", "java.lang.Deprecated");
-            put("Override", "java.lang.Override");
-            put("SuppressWarnings", "java.lang.SuppressWarnings");
+            for (final String className : new String[] { "Appendable", "CharSequence", "Cloneable", "Comparable",
+                "Iterable", "Readable", "Runnable", "Thread.UncaughtExceptionHandler", "Boolean", "Byte", "Character",
+                "Character.Subset", "Character.UnicodeBlock", "Class", "ClassLoader", "Compiler", "Double", "Enum",
+                "Float", "InheritableThreadLocal", "Integer", "Long", "Math", "Number", "Object", "Package", "Process",
+                "ProcessBuilder", "Runtime", "RuntimePermission", "SecurityManager", "Short", "StackTraceElement",
+                "StrictMath", "String", "StringBuffer", "StringBuilder", "System", "Thread", "ThreadGroup",
+                "ThreadLocal", "Throwable", "Void", "Thread.State", "ArithmeticException",
+                "ArrayIndexOutOfBoundsException", "ArrayStoreException", "ClassCastException",
+                "ClassNotFoundException", "CloneNotSupportedException", "EnumConstantNotPresentException", "Exception",
+                "IllegalAccessException", "IllegalArgumentException", "IllegalMonitorStateException",
+                "IllegalStateException", "IllegalThreadStateException", "IndexOutOfBoundsException",
+                "InstantiationException", "InterruptedException", "NegativeArraySizeException", "NoSuchFieldException",
+                "NoSuchMethodException", "NullPointerException", "NumberFormatException", "RuntimeException",
+                "SecurityException", "StringIndexOutOfBoundsException", "TypeNotPresentException",
+                "UnsupportedOperationException", "AbstractMethodError", "AssertionError", "ClassCircularityError",
+                "ClassFormatError", "Error", "ExceptionInInitializerError", "IllegalAccessError",
+                "IncompatibleClassChangeError", "InstantiationError", "InternalError", "LinkageError",
+                "NoClassDefFoundError", "NoSuchFieldError", "NoSuchMethodError", "OutOfMemoryError",
+                "StackOverflowError", "ThreadDeath", "UnknownError", "UnsatisfiedLinkError",
+                "UnsupportedClassVersionError", "VerifyError", "VirtualMachineError", "Deprecated", "Override",
+                "SuppressWarnings" }) {
+                put(className, "java.lang." + className);
+            }
         }
     };
 
