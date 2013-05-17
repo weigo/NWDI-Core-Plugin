7862064cfcf23d49700dcf035bdcc6b49b3d741b
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2013-05-17 22:44:56 +0200

    Use UTF-8 when reading DTR change logs.

--- src/main/java/org/arachna/netweaver/hudson/nwdi/changelog/DtrChangeLogParser.java
+++ src/main/java/org/arachna/netweaver/hudson/nwdi/changelog/DtrChangeLogParser.java
@@ -9,9 +9,11 @@ import hudson.scm.ChangeLogSet;
 import hudson.scm.ChangeLogSet.Entry;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.Charset;
 
 import org.arachna.xml.DigesterHelper;
 import org.xml.sax.SAXException;
@@ -27,7 +29,7 @@ public final class DtrChangeLogParser extends ChangeLogParser {
         SAXException {
         final DtrChangeLogSet changeSet = new DtrChangeLogSet(build);
 
-        parse(changeSet, new FileReader(changelogFile));
+        parse(changeSet, new InputStreamReader(new FileInputStream(changelogFile), Charset.forName("UTF-8")));
 
         return changeSet;
     }
