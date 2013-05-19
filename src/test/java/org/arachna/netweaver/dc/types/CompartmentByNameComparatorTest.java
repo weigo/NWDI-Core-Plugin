4a6546f19559a5cca179a8493656a8c491ebb8d6
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2013-05-19 22:09:25 +0200

    Clean up unit tests

--- src/test/java/org/arachna/netweaver/dc/types/CompartmentByNameComparatorTest.java
+++ src/test/java/org/arachna/netweaver/dc/types/CompartmentByNameComparatorTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
  * Unit tests for {@link CompartmentByNameComparator}.
  * 
  * @author Dirk Weigenand
- * 
  */
 public class CompartmentByNameComparatorTest {
     /**
@@ -24,66 +23,60 @@ public class CompartmentByNameComparatorTest {
     private CompartmentByNameComparator comparator;
 
     /**
+     * An example compartment.
+     */
+    private final Compartment compartment1 = Compartment.create("example.com_ExampleCompartment_1", CompartmentState.Source);
+
+    /**
      */
     @Before
     public void setUp() {
-        this.comparator = new CompartmentByNameComparator();
+        comparator = new CompartmentByNameComparator();
     }
 
     /**
      */
     @After
     public void tearDown() {
-        this.comparator = null;
+        comparator = null;
     }
 
     /**
      * Test method for
-     * {@link org.arachna.netweaver.dc.types.CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}
-     * .
+     * {@link CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}.
      */
     @Test
     public final void testCompareNullArguments() {
-        assertThat(this.comparator.compare(null, null), is(equalTo(0)));
+        assertThat(comparator.compare(null, null), is(equalTo(0)));
     }
 
     /**
      * Test method for
-     * {@link org.arachna.netweaver.dc.types.CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}
-     * .
+     * {@link CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}.
      */
     @Test
     public final void testCompareOneCompartmentAgainstNull() {
-        final Compartment compartment =
-            new Compartment("ExampleCompartment", CompartmentState.Source, "example.com", "", "ExampleCompartment_1");
-        assertThat(this.comparator.compare(compartment, null), is(equalTo(1)));
-        assertThat(this.comparator.compare(null, compartment), is(equalTo(-1)));
+        assertThat(comparator.compare(compartment1, null), is(equalTo(1)));
+        assertThat(comparator.compare(null, compartment1), is(equalTo(-1)));
     }
 
     /**
      * Test method for
-     * {@link org.arachna.netweaver.dc.types.CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}
-     * .
+     * {@link CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}.
      */
     @Test
     public final void testCompareOneCompartmentWithSelf() {
-        final Compartment compartment =
-            new Compartment("ExampleCompartment", CompartmentState.Source, "example.com", "", "ExampleCompartment_1");
-        assertThat(this.comparator.compare(compartment, compartment), is(equalTo(0)));
+        assertThat(comparator.compare(compartment1, compartment1), is(equalTo(0)));
     }
 
     /**
      * Test method for
-     * {@link org.arachna.netweaver.dc.types.CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}
-     * .
+     * {@link CompartmentByNameComparator#compare(org.arachna.netweaver.dc.types.Compartment, org.arachna.netweaver.dc.types.Compartment)}.
      */
     @Test
     public final void testCompareTwoCompartments() {
-        final Compartment compartment1 =
-            new Compartment("ExampleCompartment", CompartmentState.Source, "example.com", "", "ExampleCompartment_1");
-        final Compartment compartment2 =
-            new Compartment("ExampleCompartment1", CompartmentState.Source, "example.com", "", "ExampleCompartment1_1");
-        assertThat(this.comparator.compare(compartment1, compartment2), is(equalTo(-1)));
-        assertThat(this.comparator.compare(compartment2, compartment1), is(equalTo(1)));
+        final Compartment compartment2 = Compartment.create("example.com_ExampleCompartment_2", CompartmentState.Source);
+        assertThat(comparator.compare(compartment1, compartment2), is(equalTo(-1)));
+        assertThat(comparator.compare(compartment2, compartment1), is(equalTo(1)));
     }
 }
