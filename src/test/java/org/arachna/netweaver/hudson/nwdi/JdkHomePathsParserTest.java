569e0c6a438864f8abcaad1ac008d34905a1f35c
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2013-06-07 23:11:32 +0200

    Correct javadoc comments.

--- src/test/java/org/arachna/netweaver/hudson/nwdi/JdkHomePathsParserTest.java
+++ src/test/java/org/arachna/netweaver/hudson/nwdi/JdkHomePathsParserTest.java
@@ -12,8 +12,9 @@ import org.arachna.netweaver.dc.types.JdkHomePaths;
 import org.junit.Test;
 
 /**
- * @author Dirk Weigenand
+ * Unit tests for {@link JdkHomePathsParser}.
  * 
+ * @author Dirk Weigenand
  */
 public class JdkHomePathsParserTest {
     /**
@@ -22,15 +23,18 @@ public class JdkHomePathsParserTest {
     private static final String HOME_FOR_JDK1_5_0 = "/opt/jdk1.5.0";
 
     /**
-     * path spec for jdk 5.
+     * path spec for jdk 1.4.
      */
     private static final String HOME_FOR_JDK1_4_2 = "/opt/jdk1.4.2";
 
     /**
-     * path spec for jdk 5.
+     * path spec for jdk 1.3.
      */
     private static final String HOME_FOR_JDK1_3_1 = "/opt/jdk1.3.1";
 
+    /**
+     * parser under test.
+     */
     private JdkHomePathsParser parser;
 
     /**
@@ -38,9 +42,9 @@ public class JdkHomePathsParserTest {
      * 
      * @param pathSpec
      *            path specifications.
-     * @return
+     * @return JdkHomePaths parsed from the given path spec.
      */
-    private JdkHomePaths getPaths(String pathSpec) {
+    private JdkHomePaths getPaths(final String pathSpec) {
         parser = new JdkHomePathsParser(pathSpec);
 
         return parser.parse();
@@ -48,22 +52,20 @@ public class JdkHomePathsParserTest {
 
     @Test
     public void testGetEmptyConfiguredJdkHomePaths() {
-        final JdkHomePaths paths = this.getPaths("");
+        final JdkHomePaths paths = getPaths("");
         assertThat(paths.getAliases(), hasSize(0));
     }
 
     @Test
     public void testGetConfiguredJdkHomePathsWithJdkOnePath() {
-        final JdkHomePaths paths =
-            this.getPaths(String.format("%s=%s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1));
+        final JdkHomePaths paths = getPaths(String.format("%s=%s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1));
         assertThat(paths.getAliases(), hasSize(1));
         assertThat(HOME_FOR_JDK1_3_1, equalTo(paths.get(JdkHomeAlias.Jdk131Home)));
     }
 
     @Test
     public void testGetConfiguredJdkHomePathsWithJdkOnePathAndLooseFormatting() {
-        final JdkHomePaths paths =
-            this.getPaths(String.format("%s = %s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1));
+        final JdkHomePaths paths = getPaths(String.format("%s = %s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1));
         assertThat(paths.getAliases(), hasSize(1));
         assertThat(HOME_FOR_JDK1_3_1, equalTo(paths.get(JdkHomeAlias.Jdk131Home)));
     }
@@ -71,8 +73,8 @@ public class JdkHomePathsParserTest {
     @Test
     public void testGetConfiguredJdkHomePathsWithSeveralJdks() {
         final JdkHomePaths paths =
-            this.getPaths(String.format("%s=%s;%s=%s,%s=%s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1,
-                JdkHomeAlias.Jdk142Home, HOME_FOR_JDK1_4_2, JdkHomeAlias.Jdk150Home, HOME_FOR_JDK1_5_0));
+            getPaths(String.format("%s=%s;%s=%s,%s=%s", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1, JdkHomeAlias.Jdk142Home,
+                HOME_FOR_JDK1_4_2, JdkHomeAlias.Jdk150Home, HOME_FOR_JDK1_5_0));
         assertThat(paths.getAliases(), hasSize(3));
         assertThat(HOME_FOR_JDK1_3_1, equalTo(paths.get(JdkHomeAlias.Jdk131Home)));
         assertThat(HOME_FOR_JDK1_4_2, equalTo(paths.get(JdkHomeAlias.Jdk142Home)));
@@ -82,9 +84,8 @@ public class JdkHomePathsParserTest {
     @Test
     public void testGetConfiguredJdkHomePathsWithSeveralJdksAndLooseFormatting() {
         final JdkHomePaths paths =
-            this.getPaths(String.format("%s= %s;%s=%s , %s = %s ", JdkHomeAlias.Jdk131Home.toString(),
-                HOME_FOR_JDK1_3_1, JdkHomeAlias.Jdk142Home, HOME_FOR_JDK1_4_2, JdkHomeAlias.Jdk150Home,
-                HOME_FOR_JDK1_5_0));
+            getPaths(String.format("%s= %s;%s=%s , %s = %s ", JdkHomeAlias.Jdk131Home.toString(), HOME_FOR_JDK1_3_1,
+                JdkHomeAlias.Jdk142Home, HOME_FOR_JDK1_4_2, JdkHomeAlias.Jdk150Home, HOME_FOR_JDK1_5_0));
         assertThat(paths.getAliases(), hasSize(3));
         assertThat(HOME_FOR_JDK1_3_1, equalTo(paths.get(JdkHomeAlias.Jdk131Home)));
         assertThat(HOME_FOR_JDK1_4_2, equalTo(paths.get(JdkHomeAlias.Jdk142Home)));
@@ -93,21 +94,21 @@ public class JdkHomePathsParserTest {
 
     @Test
     public void testParsingCollectsInvalidJdkHomes() {
-        this.getPaths("xyz");
-        assertThat(this.parser.hasInvalidJdkHomeNames(), equalTo(true));
+        getPaths("xyz");
+        assertThat(parser.hasInvalidJdkHomeNames(), equalTo(true));
     }
 
     @Test
     public void testParserFormatsInvalidJdkHomes() {
-        String expected = "xyz";
-        this.getPaths(expected);
-        assertThat(this.parser.getInvalidJdkHomeNames(), equalTo(expected));
+        final String expected = "xyz";
+        getPaths(expected);
+        assertThat(parser.getInvalidJdkHomeNames(), equalTo(expected));
     }
 
     @Test
     public void testParserFormatsMultipleInvalidJdkHomes() {
-        String expected = "abc, xyz";
-        this.getPaths("xyz=/opt/jdk1.5;abc=/opt/jdk1.6");
-        assertThat(this.parser.getInvalidJdkHomeNames(), equalTo(expected));
+        final String expected = "abc, xyz";
+        getPaths("xyz=/opt/jdk1.5;abc=/opt/jdk1.6");
+        assertThat(parser.getInvalidJdkHomeNames(), equalTo(expected));
     }
 }
