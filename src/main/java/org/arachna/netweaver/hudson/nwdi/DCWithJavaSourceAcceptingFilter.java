f56245f7c580100ddf5efdaa5de591dd069a59a1
Dirk Weigenand <Dirk.Weigenand@gmx.de>
2013-06-10 21:39:46 +0200

    add missing parameter javadoc.

--- src/main/java/org/arachna/netweaver/hudson/nwdi/DCWithJavaSourceAcceptingFilter.java
+++ src/main/java/org/arachna/netweaver/hudson/nwdi/DCWithJavaSourceAcceptingFilter.java
@@ -4,24 +4,23 @@
 package org.arachna.netweaver.hudson.nwdi;
 
 import org.arachna.netweaver.dc.types.DevelopmentComponent;
+import org.arachna.netweaver.dc.types.DevelopmentComponentType;
 
 /**
- * Check whether a development component can contain java sources based on its
- * {@link DevelopmentComponentType}.
- *
+ * Check whether a development component can contain java sources based on its {@link DevelopmentComponentType}.
+ * 
  * @author Dirk Weigenand
  */
 public final class DCWithJavaSourceAcceptingFilter implements IDevelopmentComponentFilter {
-
     /**
-     * Accept the given component iff its {@link DevelopmentComponentType}
-     * indicates that it can contain Java sources.
-     *
-     * @return <code>true</code> iff the given DCs
-     *         {@link DevelopmentComponentType} indicates that it can contain
-     *         Java sources, <code>false</code> otherwise.
+     * Accept the given component iff its {@link DevelopmentComponentType} indicates that it can contain Java sources.
+     * 
+     * @param component
+     *            the component to be filtered for potentially containing java sources.
+     * @return <code>true</code> iff the given DCs {@link DevelopmentComponentType} indicates that it can contain Java sources,
+     *         <code>false</code> otherwise.
      */
-    public boolean accept(DevelopmentComponent component) {
+    public boolean accept(final DevelopmentComponent component) {
         return component != null && component.getType().canContainJavaSources();
     }
 }
